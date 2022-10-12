96a6308146af19f0d4ee49aaea55c3f311afc13d
Andrew Webb <andrew.webb@ibm.com>
2022-10-12 15:39:35 -0500

    creating repo

---
+++ src/main/java/JavaEELearning/com/example/JavaEELearning/JavaEeLearningApplication.java
@@ -0,0 +1,13 @@
+package JavaEELearning.com.example.JavaEELearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEeLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEeLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JavaEELearning/com/example/JavaEELearning/JavaEeLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaEELearning.com.example.JavaEELearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEeLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
